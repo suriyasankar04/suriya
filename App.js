ef5e0ae5a8e68bf2439a20cce70c82be4f070d62
suriyasankar04 <87844548+suriyasankar04@users.noreply.github.com>
2022-12-13 10:30:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gallery from './gallery';
+import './gallery';
+function App() {
+  return (
+    <div>
+      <Gallery/>
+       
+    </div>
+
+  )
+}
+
+export default App;
